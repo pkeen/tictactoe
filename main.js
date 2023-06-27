ba8d4acd4093860f5bd57aaff16aadbad6bea233
pkeen <pkeen7@gmail.com>
2023-06-27 00:04:35 -0600

    add logic to determine winner

--- main.js
+++ main.js
@@ -12,23 +12,60 @@ const boardSection = document.getElementById('board');
 
 /*----- functions -----*/
 
-const determineWinner = () => {
-    
+const checkDiagonalWinUp = (rowIdx, colIdx) => {
+    // create array of containing diagonal upward from 0,2 values
+    const diagUpArray = [];
+    for (let i = 0, j = board.length -1; i < board.length; i++, j-- ) {
+        diagUpArray.push(board[i][j])
+    }
+    return diagUpArray.every(val => val === board[rowIdx][colIdx]);
+}
+
+const checkDiagonalWinDown = (rowIdx, colIdx) => {
+    // create array containing diagonal downward values from 0,0
+    const diagDownArray = board.map((r, i) => board[i][i]);
+    return diagDownArray.every(val => val === board[rowIdx][colIdx]);
+}
+
+const checkColWin = (rowIdx, colIdx) => {
+    // create a array representing column currently in
+    const colArray = board.map((r, i) => board[i][colIdx]);
+    // every value in colArray equal to current cell value
+    return colArray.every(val => val === board[rowIdx][colIdx])
+}
+
+const checkRowWin = (rowIdx, colIdx) => {
+    // every value in current row is equal to current cell value
+    return board[rowIdx].every(val => val === board[rowIdx][colIdx])
+}
+
+const checkWinner = (rowIdx, colIdx) => {
+    return checkRowWin(rowIdx, colIdx) || 
+        checkColWin(rowIdx, colIdx) || 
+        checkDiagonalWinDown(rowIdx, colIdx) || 
+        checkDiagonalWinUp(rowIdx, colIdx);
 }
 
 const handleClick = ({target}) => {
     // shorthand var
-    const idSplit = target.id.split("-");
-    const cellPos = {
-        [idSplit[0][0]]: idSplit[0][1],
-        [idSplit[1][0]]: idSplit[1][1]
-    }
+    // const idSplit = target.id.split("-");
+    // const cellPos = {
+    //     [idSplit[0][0]]: idSplit[0][1],
+    //     [idSplit[1][0]]: idSplit[1][1]
+    // }
+    const rowIdx = target.id.split("-")[0];
+    const colIdx = target.id.split("-")[1];
+
     // update board data
-    board[cellPos.r][cellPos.c] = playerTurn;
+    board[rowIdx][colIdx] = playerTurn;
+    // check if winner
+    winner = checkWinner(rowIdx, colIdx);
+
+    console.log(winner);
+    
     // switch players
     playerTurn = playerTurn === PLAYER1 ? PLAYER2 : PLAYER1;
-    // check if winner
-    winner = determineWinner();
+    
     // re-render
     render();
 
@@ -39,7 +76,7 @@ const renderBoard = () => {
     board.forEach((row, rowIdx) => {
         row.forEach((col, colIdx) => {
             const cell = document.createElement('div');
-            cell.setAttribute('id', `r${rowIdx}-c${colIdx}`);
+            cell.setAttribute('id', `${rowIdx}-${colIdx}`);
             cell.classList.add('cell');
             cell.innerText = board[rowIdx][colIdx];
             boardSection.append(cell);
@@ -59,6 +96,7 @@ const init = () => {
     ];
 
     playerTurn = PLAYER1;
+    winner = false;
 
     render();
 }
